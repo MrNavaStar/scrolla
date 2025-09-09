4be697409d2c5a4573a040cb98345bb10d7ec98e
MrNavaStar <mr.navastar@gmail.com>
2025-09-09 02:41:42 -0600

    improved terminal handling

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 use std::{env, io, thread, time::Duration};
+use std::io::Write;
+use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, Ordering};
 
 fn main() {
+    let running = Arc::new(AtomicBool::new(true));
+    let r = running.clone();
+    ctrlc::set_handler(move || {
+        r.store(false, Ordering::SeqCst);
+    }).expect("Error setting Ctrl-C handler");
+
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 2 {
@@ -34,12 +43,18 @@ fn main() {
             }
         }
     }
-    
+
     let chars: Vec<char> = (text_args.join(" ") + &*' '.to_string().repeat(padding))
         .chars().collect();
+    
+    // Clear Terminal
+    print!("\x1B[?1049h");   // Switch to alternate buffer
+    print!("\x1B[2J\x1B[H"); // clear screen and move cursor to top-left
+    print!("\x1B[?25l");     // hide cursor
+    io::stdout().flush().unwrap();
 
     let mut pos = 0;
-    loop {
+    while running.load(Ordering::SeqCst) {
         let mut view = String::new();
         for i in 0..width {
             let idx = (pos + i) % chars.len();
@@ -52,4 +67,9 @@ fn main() {
         thread::sleep(Duration::from_millis(delay));
         pos = (pos + 1) % chars.len();
     }
+
+    // Restore terminal
+    print!("\x1B[?25h");    // show cursor
+    print!("\x1B[?1049l");  // Switch back to original buffer
+    io::stdout().flush().unwrap();
 }
